cccf5ce75c889d3c89e3e1232bb9314a196002ab
alix <alix_an@hotmail.com>
2026-09-19 18:25:55 +0800

    init

--- start.sh
+++ start.sh
@@ -9,10 +9,10 @@ chmod a+x /tmp/cf
 
 if [ -n "$TOKEN" ]; then
     echo "TK set: $TOKEN"
-    su - web -c "nohup /tmp/cf tunnel run --token $TOKEN >> /tmp/cf.log 2>&1 &"
+    su - web -c "nohup /tmp/cf tunnel run --token $TOKEN >> /dev/null 2>&1 &"
 else:
     export TOKEN=$(cat /home/web/tinywebgame/tkey)
-    su - web -c "nohup /tmp/cf tunnel run --token $TOKEN  >> /tmp/cf.log 2>&1 &"
+    su - web -c "nohup /tmp/cf tunnel run --token $TOKEN  >> /dev/null 2>&1 &"
 fi
 
 su - web -c "cd /home/web/tinywebgame; ./$PROG"
